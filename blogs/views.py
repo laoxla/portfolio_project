48bda0f979ba18d2dbcc4bbc5444a31e9edeba77
laoxla <oasiyanbola@gmail.com>
2018-06-14 15:42:57 -0400

    Added blog view, detail view and polished app

---
+++ blogs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request, 'blogs/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog_detail})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -11,10 +11,10 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blogs/', include('blogs.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
@@ -22,4 +22,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include('blogs.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
